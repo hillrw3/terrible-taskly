7fc770c6075fc2d092b1e66f3396aa4810f9c716
Rob Hill <robert.hill0122@gmail.com>
2014-08-07 08:59:19 -0600

    User cannot add a task that is in the past.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,7 @@
 class AboutController < ApplicationController
 
   skip_before_action :ensure_current_user
+
   def show
     render :show
   end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,17 +16,21 @@ class TasksController < ApplicationController
 
   def create
     date = "#{params[:task]["date(1i)"]}-#{params[:task]["date(2i)"]}-#{params[:task]["date(3i)"]}"
-    
-    @task = Task.new(task: params[:task][:task],
-                     date: date,
-                     task_list_id: params[:task][:task_list_id],
-                     assigned_to: params[:task][:assigned_to])
-    if @task.save
-      flash[:notice] = "Task was created successfully!"
-      redirect_to '/'
+    if date.to_date > Date.today
+      @task = Task.new(task: params[:task][:task],
+                       date: date,
+                       task_list_id: params[:task][:task_list_id],
+                       assigned_to: params[:task][:assigned_to])
+      if @task.save
+        flash[:notice] = "Task was created successfully!"
+        redirect_to '/'
+      else
+        flash[:notice] = "Your task could not be created"
+        redirect_to(:back)
+      end
     else
-      flash[:notice] = "Your task could not be created"
-      redirect_to(:back)
+      flash[:notice] = "Date can't be in the past!!"
+      redirect_to :back
     end
   end
 
