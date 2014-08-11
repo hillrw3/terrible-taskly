2bc96ea214acbf065e2f487edc1d6e800ef910a0
Rob Hill <robert.hill0122@gmail.com>
2014-08-11 14:58:05 -0600

    making taskly terrible

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,18 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+
+  var links = $('a');
+  links.hover(function(){
+    $(this).toggleClass('hidden');
+  });
+
+  links.on('click',function(){
+    $('div').css('background-image', 'url(http://www.reactionface.info/sites/default/files/imagecache/Node_Page/images/4e584a22eddc6c06410053e4.jpg)');
+  });
+
+  
+
+});
